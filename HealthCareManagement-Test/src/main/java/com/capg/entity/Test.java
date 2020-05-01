1149eaba9fbee690ca6845b43fc34c38ebdb244d
SoumyaReddy <SoumyaReddy@LAPTOP-T3CKGQ3T>
2020-05-01 10:27:14 +0530

    commit

--- HealthCareManagement-Test/src/main/java/com/capg/entity/Test.java
+++ HealthCareManagement-Test/src/main/java/com/capg/entity/Test.java
@@ -6,6 +6,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
+import com.cg.hcsAdmin.dto.Diagnosticcenter;
+
 @Entity
 public class Test 
 {
@@ -14,20 +16,21 @@ public class Test
 	private Integer testId;
 	@Column(name = "test_name")
 	private String test_name;
+	@OneToOne
+    @JoinColumn(name = "center_id")
+	private Diagnosticcenter diagnosticcenter;
 	
+
 	public Test() {	}
 	
 
-	public Test(Integer testId, String test_name) {
+	public Test(Integer testId, String test_name, Diagnosticcenter diagnosticcenter) {
 		super();
 		this.testId = testId;
 		this.test_name = test_name;
 		
-		
-	}
-
-
-	public Integer getTestId() {
+		}
+    public Integer getTestId() {
 		return testId;
 	}
 
@@ -38,13 +41,18 @@ public class Test
 
 	public String getTest_name() {
 		return test_name;
+	
 	}
-
-	public void setTest_name(String test_name) {
+     public void setTest_name(String test_name) {
 		this.test_name = test_name;
+		
 	}
+     public Diagnosticcenter getDiagnosticcenter() {
+ 		return diagnosticcenter;
+ 	}
 
-	
 
-	
+ 	public void setDiagnosticcenter(Diagnosticcenter diagnosticcenter) {
+ 		this.diagnosticcenter = diagnosticcenter;
+ 	}
 }
