f5367789837fb4a482fa5fe9f71ff0e5585fe0d3
VangalaSowmyaReddy <soumyavangala4@gmail.com>
2020-05-05 14:21:39 +0530

    commit

--- HealthCareManagement-Test/src/main/java/com/capg/TestApplication.java
+++ HealthCareManagement-Test/src/main/java/com/capg/TestApplication.java
@@ -2,8 +2,9 @@ package com.capg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
-
+@EnableEurekaClient
 public class TestApplication {
 	
 		public static void main(String[] args) {
