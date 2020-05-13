0a638569156ad671767324ffc5a8030652ffac41
VangalaSowmyaReddy <soumyavangala4@gmail.com>
2020-05-13 19:40:48 +0530

    commit

--- HealthCareManagementSystemTest/src/main/java/com/cap/anurag/HealthCareSystemApplication.java
+++ HealthCareManagementSystemTest/src/main/java/com/cap/anurag/HealthCareSystemApplication.java
@@ -3,9 +3,10 @@ package com.cap.anurag;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class HealthCareSystemApplication {
 
 	public static void main(String[] args) {
